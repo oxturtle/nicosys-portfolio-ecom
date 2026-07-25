11cb6827fdbdc9ac759a1d7de100fa2a9352b414
Nico <nrafael@gmail.com>
2026-07-25 14:35:28 -0700

    Updated sql code

--- sql/nicosys-portfolio-ecom-sql.sql
+++ sql/nicosys-portfolio-ecom-sql.sql
@@ -1,136 +1,216 @@
---synthetic squarespace data
-Select 
-	*
-From 
-	squarespace_s
-;
+/*
+Business Question
+Stakeholder
+Purpose
+SQL Query
+Findings:
+Business Insight 
+*/
 
 
 
+
+
+
+
+/*
+Business Question:
+What is the total gross and net sales from Squarespace and TikTok Shop?
+
+Stakeholder:
+Owner of e-commerce business
+
+Purpose:
+To see what the business total gross and net sales are from Squarespace platfrom.
+
+SQL Query: Query Below
+
+Findings:
+Squarespace: Total Gross Sales: $379,500.00 | Total Net Sales: $339,287.77
+TikTok shop export only contained gross sales: Total Gross Sales $968,587.16
+
+Business Insight:
+TikTok Shop generates 74% of business sales while Squarespace generates the remaining 26%.
+TikTok shop didn't have gross sales data. Squarespace gross sales were 
+
+*/
+
+--Total Gross and Net Squarespace sales
 Select
-	sum(gross_sales) as total_gross_sales_ss
+	  sum(gross_sales) as total_gross_sales_ss
+	, sum(net_sales) as total_net_sales_ss
+	, sum(gross_sales) - sum(net_sales) as total_taxes_and_fees
 From
 	squarespace_s
 ;
 
+
+--total tiktok sales
 Select
-	sum(net_sales) as total_net_sales_ss
+	sum(total_sales) as total_gross_tiktok_sales
 From
-	squarespace_s
+	tiktok_s
 ;
+
+--Sales by Platform and Total Sales Overall 
+Select
+	  sum(total_tts_sales_clean) as total_tts_sales
+	, sum(total_ss_sales) as total_ss_sales
+	, sum(total_tts_sales_clean) + sum(total_ss_sales) as total_biz_sales 
+From
+	(--Query showing daily sales between TikTok Shop and Squarespace
+		Select
+			  ss.order_date as order_date --Since Squarespace data has more days of year reported, will use date field to represent both shops
+			  , sum(ss.net_sales) as total_ss_sales
+			--, tts.order_date as tt_order_date --commented out becuase we only need 1 date column to represent each store
+			--, tt.total_sales as total_tt_sales --commented out due to null values
+			, coalesce (tts.total_sales,0) as total_tts_sales_clean
+			, sum(ss.net_sales)+(coalesce (tts.total_sales,0)) as total_sales_overall
+
+		From
+			squarespace_s ss
+				left join tiktok_s as tts
+					on ss.order_date=tts.order_date
+		Group By
+			  ss.order_date
+			, tts.order_date
+			, tts.total_sales
+		Order By
+			ss.order_date
+	)
+;
+
+
+/*
+Business Question:
+What are the top selling products?
+
+Stakeholder:
+Owner of e-commerce business
+
+Purpose:
+To define which products are selling most. 
+
+SQL Query: query below 
+
+Findings:
+
+Business Insight 
+*/
 
 --sales by product
 Select
-	  sum(gross_sales) as total_sales_by_product
+	  sum(gross_sales) as total_sales_by_product
 	, product_name
-
 From
 	squarespace_s
-	
-Group By
+Group by
 	product_name
-
 Order By
 	total_sales_by_product desc
 ;
 
 
---sales by state
-Select
-	  sum(gross_sales) as total_sales_by_product
-	, state_abv
 
+
+/*
+Business Question
+Stakeholder
+Purpose
+SQL Query
+Findings:
+Business Insight 
+*/
+
+
+--Total sales by state
+Select 
+	  sum(ss.gross_sales) as total_sales_by_product
+	, ss.state
 From
-	squarespace_s
-	
+	squarespace_s ss
 Group By
-	state_abv
-
-Order by
+	ss.state
+Order By
 	total_sales_by_product desc
 ;
 
-
---sales by state
+--Top 3 Highest Sales by State
 Select
 	sum(total_sales_by_product)
-
-From (
+From 
+	(
 		Select
-			  sum(gross_sales) as total_sales_by_product
-			, state_abv
-			, product_name
-		
+			  sum(ss.gross_sales) as total_sales_by_product
+			, ss.state
+			, ss.product_name
+
 		From
-			squarespace_s
-			
+			squarespace_s ss
+		
 		Where
-			product_name != 'Anime Mystery Box' 
+			product_name != 'Anime Mystery Box'
 			and product_name != 'Naruto Hoodie'
-		
+
 		Group By
-			  state_abv
+			  ss.state
 			, product_name
-		
 		Having
-			--state_abv = 'Texas'
-			--state_abv = 'California'
-			state_abv = 'Florida'
-		
-		Order by
+			ss.state in ('Texas', 'California', 'Florida')
+
+		Order By
 			total_sales_by_product desc
 	)
 ;
 
 
 
+
 --total ss refunds
 Select
-	  sum(refund_amount) as total_ss_refund
+	sum(ss.refund_amount) as total_ss_refund
 
 From
-	squarespace_s
+	squarespace_s ss
 ;
 
 
 --total ss discount
 Select
-	  sum(discount_amount) as total_refund
+	sum(ss.discount_amount) as total_refund
 
 From
-	squarespace_s
+	squarespace_s ss
 ;
 
 
 
 --total ss and tt sales by day
 Select
-	sum(total_sales_overall) as total_between_platforms
-
-From (
-		Select 
-			  ss.order_date as ss_order_date
-			, tts.order_date as tiktok_order_date
+	sum(total_sales_overall) as total_tts_and_ss
+From
+	(--the subquery code to show combined order dates and totals between ss and tts
+		Select
+			  ss.order_date as ss_order_date
+			, tts.order_date as tt_order_date
 			, sum(ss.net_sales) as total_ss_sales
-			--, tts.total_sales as total_tiktok_sales --Commented out due to null values
-			, coalesce (tts.total_sales, 0) as not_null_total_tiktok_sales
-			, sum(ss.net_sales) + (coalesce (tts.total_sales, 0)) as total_sales_overall
-		
-		From 
+			--, tt.total_sales as total_tt_sales --commented out due to null values
+			, coalesce (tts.total_sales,0) as not_null_total_tts_sales
+			, sum(ss.net_sales)+(coalesce (tts.total_sales,0)) as total_sales_overall
+
+		From
 			squarespace_s ss
 				left join tiktok_s as tts
-					on ss.order_date = tts.order_date 
-		
+					on ss.order_date=tts.order_date
 		Group By
-			  ss.order_date
-			, tiktok_order_date
+			  ss.order_date
+			, tts.order_date
 			, tts.total_sales
-		
 		Order By
-			ss.order_date 
+			ss.order_date
 	)
-
 ;
+
 
 
 --synthetic tiktok data
@@ -140,12 +220,7 @@ From
 	tiktok_s
 ;
 
---total tiktok sales
-Select
-	sum(total_sales) as total_tiktok_sales
-From
-	tiktok_s
-;
+
 
 
 
